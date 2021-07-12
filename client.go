f9e4ba121d2623a8d259f1df79cd062d254855af
rockstar-0000 <tekbrucejin@gmail.com>
2021-07-13 00:33:47 +0200

    initial commit

---
+++ client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
+}
