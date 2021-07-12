42fff4eb547dc8f12b6de22acc000e39cefc75fa
rockstar-0000 <tekbrucejin@gmail.com>
2021-07-13 00:58:34 +0200

    account balance

--- client.go
+++ client.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-	client, err := ethclient.Dial("https://mainnet.infura.io")
+	client, err := ethclient.Dial("https://eth-mainnet.alchemyapi.io/v2/oojMfZkYjgc64EeNl3uQCgnesgLiojvu")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("we have a connection")
-	_ = client // we'll use this in the upcoming sections
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893860171173005034
+
+	blockNumber := big.NewInt(5532993)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 25729324269165216042
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 25.729324269165216041
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
 }
